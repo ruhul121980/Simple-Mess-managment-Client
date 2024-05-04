2d3e17b3391899244d0c5bd6c7192ec49d37940d
unknown <ruhul121980@gmail.com>
2024-05-04 22:31:00 +0600

    add some link

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 
 
 const Error404 = () => {
   return (
     <div>
       <div className="font-bold text-5xl text-center flex justify-center">404 - Not Found</div>
+
+      <Link to='/'><button>Go Back to Home</button></Link>
       
     </div>
   );
